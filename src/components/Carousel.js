fa3ace277428d737408fa04f7156efe8c69f482c
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-03-26 17:27:35 -0300

    First Commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,11 +1,18 @@
 // src/components/Carousel.js
 
 import React from 'react';
+import './Carousel.css'; // Importar estilos CSS
+import foto400 from '../img/Foto400.png'; // Importar la imagen
 
 function Carousel() {
   return (
-    <div className="carousel">
-      {/* Aquí va tu carrusel de fotos */}
+    <div className="carousel-container">
+      {/* Replicar la imagen cinco veces */}
+      <img src={foto400} alt="Foto" className="carousel-image" />
+      <img src={foto400} alt="Foto" className="carousel-image" />
+      <img src={foto400} alt="Foto" className="carousel-image" />
+      <img src={foto400} alt="Foto" className="carousel-image" />
+      <img src={foto400} alt="Foto" className="carousel-image" />
     </div>
   );
 }
