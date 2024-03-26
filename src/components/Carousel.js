67ba98982943d5b41bb46542c251144519fcc7d4
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-03-26 17:37:15 -0300

    First Commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,18 +1,31 @@
 // src/components/Carousel.js
 
-import React from 'react';
-import './Carousel.css'; // Importar estilos CSS
-import foto400 from '../img/Foto400.png'; // Importar la imagen
+import React, { useState, useEffect } from 'react';
+import './Carousel.css';
+import foto400 from '../img/Foto400.png';
 
 function Carousel() {
+  const [currentIndex, setCurrentIndex] = useState(2); // Índice inicial para la imagen del medio
+
+  useEffect(() => {
+    const interval = setInterval(() => {
+      // Cambiar al siguiente índice
+      setCurrentIndex(currentIndex => (currentIndex + 1) % 5);
+    }, 3000); // Cambiar cada 3 segundos
+
+    return () => clearInterval(interval); // Limpiar el intervalo cuando el componente se desmonta
+  }, []);
+
   return (
     <div className="carousel-container">
-      {/* Replicar la imagen cinco veces */}
-      <img src={foto400} alt="Foto" className="carousel-image" />
-      <img src={foto400} alt="Foto" className="carousel-image" />
-      <img src={foto400} alt="Foto" className="carousel-image" />
-      <img src={foto400} alt="Foto" className="carousel-image" />
-      <img src={foto400} alt="Foto" className="carousel-image" />
+      {[0, 1, 2, 3, 4].map(index => (
+        <img
+          key={index}
+          src={foto400}
+          alt="Foto"
+          className={`carousel-image ${index === currentIndex ? 'center-image' : ''}`}
+        />
+      ))}
     </div>
   );
 }
