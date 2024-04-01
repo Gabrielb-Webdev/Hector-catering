0469150c6c3263264cea7096ae2d0454fb34819e
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-04-01 02:05:19 -0300

    First Commit

--- src/components/Carousel.js
+++ src/components/Carousel.js
@@ -1,13 +1,37 @@
-// src/components/ContactForm.js
+// src/components/Carousel.js
 
 import React from 'react';
+import Slider from 'react-slick';
+import './Carousel.css'; // Importa el archivo CSS para los estilos personalizados
 
-function ContactForm() {
+const Carousel = () => {
+  const settings = {
+    dots: true,
+    infinite: true,
+    speed: 500,
+    slidesToShow: 1,
+    slidesToScroll: 1,
+    arrows: false, // Desactiva las flechas de navegación
+    autoplay: true, // Activa la reproducción automática
+    autoplaySpeed: 5000 // Establece el intervalo de tiempo en milisegundos (5 segundos)
+  };
+
   return (
-    <section className="contact-form">
-      {/* Aquí va tu formulario de contacto */}
-    </section>
+    <div className="carousel-container">
+      <Slider {...settings}>
+        <div>
+          <img src="images/large_image2.png" alt="Imagen 1" />
+        </div>
+        <div>
+          <img src="images/large_image2.png" alt="Imagen 2" />
+        </div>
+        <div>
+          <img src="images/large_image2.png" alt="Imagen 3" />
+        </div>
+        {/* Agrega más elementos div con imágenes según sea necesario */}
+      </Slider>
+    </div>
   );
 }
 
-export default ContactForm;
+export default Carousel;
