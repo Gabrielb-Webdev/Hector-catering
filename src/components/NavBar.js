e23263d846390a73b13d53fa52252e49f02a2abb
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-04-02 14:08:22 -0300

    Done

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,36 @@
 // src/components/NavBar.js
 
-import React from 'react';
+import React, { useState } from 'react';
 import logo from '../img/LOGO.png'; // Importar la imagen
 import './NavBar.css'; // Importar estilos CSS
 
 function NavBar() {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+    // Agregar clase al navbar cuando se abre el menú
+    document.querySelector('.navbar').classList.toggle('menu-open');
+  };
+
   return (
     <nav className="navbar">
       <>
         <link rel="preconnect" href="https://fonts.googleapis.com" />
-        <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin />
+        <link rel="preconnect" href="https://fonts.gstatic.com" crossOrigin />
         <link href="https://fonts.googleapis.com/css2?family=Merienda:wght@300..900&display=swap" rel="stylesheet" />
       </>
       <div className="navbar-left">
         <img src={logo} alt="Logo" />
       </div>
-      <div className="navbar-right">
+      <div className={`navbar-right ${isOpen ? 'open' : ''}`}>
+        {/* Botón de hamburguesa */}
+        <div className={`hamburger-menu ${isOpen ? 'open' : ''}`} onClick={toggleMenu}>
+          <span></span>
+          <span></span>
+          <span></span>
+        </div>
+        {/* Menú de navegación */}
         <ul>
           <li>Inicio</li>
           <li>Productos</li>
@@ -26,4 +41,4 @@ function NavBar() {
   );
 }
 
-export default NavBar;
+export default NavBar; // Agregar esta línea para exportar el componente NavBar por defecto
