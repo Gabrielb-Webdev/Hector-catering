051a58d92407cf81c3c54f2954b23f3d7929325a
Gabrielbg211 <Gbustosgarcia01@gmail.com>
2024-04-08 14:55:53 -0300

    Done

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -27,7 +27,10 @@ function AboutUs() {
           <img src={newLeftImage} alt="Nueva imagen izquierda" />
         </div>
         <div className="paragraph">
-          <p>{lorem.generateParagraphs(1)}</p>
+          Somos una empresa con más de 14 años dedicados a ofrecer el mejor servicio para distintos eventos con un gran equipo de profesionales en todas las áreas que se requieren. 
+          Tenemos distintas opciones en las que preparamos a las necesidades de nuestros clientes.
+          Nuestra experiencia de trabajar en grandes restaurantes   en embajadas como la de Chile, Perú , Uruguay y sobre todo estar en este  hermoso país que es Argentina las cuales nos da la opción detener variedades de asados con las más finas carnes el gusto de cliente y sobre todo funcionando los bocaditos  y productos internacionales.
+          No lo dude será una experiencia inolvidable para tu evento!
         </div>
         <div className="new-right-image">
           <img src={newRightImage} alt="Nueva imagen derecha" />
--- src/components/ProductDetail.js
+++ src/components/ProductDetail.js
@@ -1,25 +1,42 @@
+// src/components/ProductDetail.js
+
 import React from "react";
 import "./ProductDetail.css";
+import leftImage from '../img/Bigote-izquierdo.png';
+import rightImage from '../img/Bigote_derecho.png';
 import newLeftImage from '../img/Tenedor.png';
 import newRightImage from '../img/Cuchillo.png';
 
 const ProductDetail = ({ product, onClose }) => {
   return (
-    <div className="product-detail-container">
-      <div className="product-detail-left">
-        <div className="product-title-container">
-          <img className="product-title-left" src={newLeftImage} alt="Tenedor" />
-          <h2>{product.title}</h2>
-          <img className="product-title-right" src={newRightImage} alt="Cuchillo" />
+    <div>
+      {/* Botón de cierre */}
+      <button className="close-button" onClick={onClose}>X</button>
+
+      <div className="product-detail-container">
+        <div className="product-detail-left">
+          <div className="product-title-container">
+            <img className="product-title-left" src={newLeftImage} alt="Tenedor" />
+            <h2>{product.title}</h2>
+            <img className="product-title-right" src={newRightImage} alt="Cuchillo" />
+          </div>
+          <img className="product-image" src={product.src} alt={product.alt} />
+          <h3>{product.description}</h3>
+          <button className="bimageInfo">Botón adicional</button>
         </div>
-        <img className="product-image" src={product.src} alt={product.alt} />
-        <h3>{product.description}</h3>
-        <button className="bimageInfo">Botón adicional</button>
-      </div>
-      <div className="product-detail-right">
-        <h4>Otro subtítulo si es necesario</h4>
-        <h3>{product.description}</h3>
-        <button className="bimageInfo">Botón adicional</button>
+        <div className="product-detail-right">
+          <div className="p-content">
+            <div className="image-left">
+              <img className='bigote' src={leftImage} alt="Bigote izquierdo" />
+            </div>
+            <h2>Contiene</h2>
+            <div className="image-right">
+              <img className='bigote' src={rightImage} alt="Bigote derecho" />
+          </div>
+        </div>
+          <h3>{product.description}</h3>
+          <button className="bimageInfo">Botón adicional</button>
+        </div>
       </div>
     </div>
   );
